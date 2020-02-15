4612d7807b4bc356926f9d86c4c282341f6475a0
vanya amany 13 <vanya@gmail.com>
2020-02-15 11:14:36 +0700

    first commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,20 @@
+@extends('layout/main');
+
+@section('title','Web Progaming UNPAS');
+
+@section('container');
+    
+
+  <div class="container">
+    <div class="row">
+      <div class="col-10">
+      <h1 class="mt-3">Hello, vanyaa !</h1>
+    </div>
+  </div>
+</div>
+
+@endsection
+
+    
+
+   
